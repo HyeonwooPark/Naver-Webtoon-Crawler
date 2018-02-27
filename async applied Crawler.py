44876555b206206ce2258df4bad4178543484754
Hyeonwoo Park <31156875+HyeonwooPark@users.noreply.github.com>
2018-02-28 05:46:23 +0900

    main

--- async applied Crawler.py
+++ async applied Crawler.py
@@ -16,7 +16,7 @@ import time
 titleID = 183559
 title = "신의 탑"
 weekday = "mon"
-episode_number = 372     # 화 (에피소드)
+episode_number = 5     # 화 (에피소드)
 # 세팅 (변경 X)
 # ---------------------------------------------------------------------------------
 storage = f'{os.path.dirname(os.path.realpath(__file__))}\\{title}'
@@ -30,27 +30,25 @@ f = codecs.open('%s\\log.txt' % storage, 'w', 'utf-8')
 
 header = {'User-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36'}
 
-def Ssearch(TITLEID, WEEKDAY, EPISODE_NUMBER):    
-    url = f"http://comic.naver.com/webtoon/detail.nhn?titleId={TITLEID}&no={(EPISODE_NUMBER+1)}&weekday={WEEKDAY}"
-    res = requests.get(url)
-    soup = bs(res.text, "lxml")
-    wt_viewer = soup.find(class_="wt_viewer")
-    imgs = wt_viewer.find_all("img")
-    srcs = [img.get("src") for img in imgs]
 
-    return EPISODE_NUMBER, list(srcs)
-
-async def search(url):
+async def search(URL):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as res:
-            page = await res.text()    
-    return page
+        async with session.get(URL) as res:
+            page = await res.text()
+            soup = bs(page, "lxml")
+            wt_viewer = soup.find(class_="wt_viewer")
+            imgs = wt_viewer.find_all("img")
+            srcs = [img.get("src") for img in imgs]
+    return list(srcs)    
 
-async def download(epi_num, srcs):
+async def download(EPISODE_NUMBER, URL):
     i = 0
+    loop = asyncio.get_event_loop()
+
+    srcs = await loop.run_in_executor(None, search, URL)
     async with aiohttp.ClientSession() as session:
         async with session.get([src for src in srcs], headers=header) as res:
-            with open(f'{storage + "_raw"}\\{epi_num}-{i}.png', 'wb') as outfile:
+            with open(f'{storage + "_raw"}\\{EPISODE_NUMBER}-{i}.png', 'wb') as outfile:
                 outfile.write(await res.content)
                 i += 1
 
@@ -58,3 +56,11 @@ async def download(epi_num, srcs):
 
 if __name__ == "__main__":
     urls = [f"http://comic.naver.com/webtoon/detail.nhn?titleId={titleID}&no={(ep+1)}&weekday={weekday}" for ep in range(episode_number)]
+    loop = asyncio.get_event_loop()
+    try : 
+        tasks = asyncio.tasks(download(episode_number, urls))
+        loop.run_until_complete(tasks)
+    finally :
+        loop.close()
+
+
